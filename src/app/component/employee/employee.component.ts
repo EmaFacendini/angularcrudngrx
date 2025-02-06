da7b949020f5ad53bca4c99daeba660520974ae8
EmanuelFacendini <efacendini@gmail.com>
2025-02-06 01:07:55 -0300

    modificcomp

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddEmployeeComponent } from '../add-employee/add-employee.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Employee } from '../../model/Employee';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { EmployeeService } from '../../service/employee.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee',
-  imports: [MatCardModule, MatButtonModule, MatDialogModule],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatDialogModule, MatTableModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit, OnDestroy {
 
-  constructor(private dialog: MatDialog) {
+  empList: Employee[] = [];
+  datatable!:MatTableDataSource<Employee>;
+  displayedColumns: string[] = ['id', 'name', 'doj', 'role', 'salary', 'action'];
+  subscription= new Subscription();
+
+  constructor(private dialog: MatDialog, private service: EmployeeService) {
+    console.log('EmployeeService:', this.service); // Verifica que el servicio esté inyectado
+    this.GetallEmployee();
 
    }
+  ngOnDestroy(): void {
+
+    this.subscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    
+  }
+
+  GetallEmployee() {
+    let sub= this.service.GetAll().subscribe(item => {
+      this.empList = item;
+      this.datatable = new MatTableDataSource(this.empList)
+    })
+    this.subscription.add(sub);
+  }
     addEmployee() {
         this.dialog.open(AddEmployeeComponent, {
           width: '50%',
